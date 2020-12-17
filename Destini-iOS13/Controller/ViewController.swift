b4138650b04bb29205afbf7c6b6f6358010b7629
Michael Dinh <tonkoalsce@gmail.com>
2020-12-17 11:36:18 +1100

    Finished story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -18,11 +18,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        choice1Button.tag = 1
+        choice2Button.tag = 2
         updateUI()
     }
     
     @IBAction func choiceButtonPressed(_ sender: UIButton) {
-        if sender == choice1Button {
+        if sender.tag == 1 {
             storyBrain.goLeft()
         } else {
             storyBrain.goRight()
@@ -32,8 +34,7 @@ class ViewController: UIViewController {
     
     
     func updateUI() {
-        print(storyBrain.currentPos)
-        print(storyBrain.storyElements[storyBrain.currentPos])
+        print(storyBrain.getCurrentText())
         storyLabel.text = storyBrain.getCurrentText()
         choice1Button.setTitle(storyBrain.getChoiceOne(), for: .normal)
         choice2Button.setTitle(storyBrain.getChoiceTwo(), for: .normal)
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -21,8 +21,8 @@ struct StoryBrain {
              Story(story: "You edge yourself deeper into the leaves, they crinkle excitedly, acceptedely knowing that ypu have chosen to lie within them; you edge towards a sleep but notice an injured bird perched upon a baren branch...", choiceOne: "Approach the bird directly", choiceTwo: "Brandish your bow and survey your surrounding"),
 
              Story(story: "As you pace yourself towards the injured doe, you notice its laboured breathing...", choiceOne: "Perfect, you extend your knife and end its suffering, you will need its meat to get out of this forest.", choiceTwo: "Care for the Doe, bandage its injuries. You become friends with the woodland animal, and she/he follows you around like a dog would."),
-             Story(story: "There are strange marking on the tree, a footprint on the soft soil warns you of a bear. A bear suddenly tunnels its way from the ground and grabs your ankles, offering you \" fresh baked goods, if you come along \". What do you do?", choiceOne: "Slash down severing the bears hands from your feet. Another one bites the dust. You moonwalk your way out of the forest.", choiceTwo: "Fresh baked goods? If never had I ever said no! You follow the bear to his house, leaving the weeping doe behind to enjoy a plate of fresh baked good. You baked as hell."),
-             Story(story: "As you walk to the eagle it swoops down and pecks at your shirt", choiceOne: "You dodge nimbly and grabe it by its nape and then throw it up into the air, followed by a swift karate chop back down into the ground. You jump up and then do a sick elbow drop. Mexican music plays in the background.", choiceTwo: "You endure its frivilous pecking. no power without pain. You relish in this ordeal, while metal music plays in the bacground and blood begins to streak across your face."),
+             Story(story: "There are strange marking on the tree, a footprint on the soft soil warns you of a bear. A bear suddenly tunnels its way from the ground and grabs your ankles, offering you \" fresh baked goods, if you come along \". What do you do?", choiceOne: "Slash down severing the bears hands from your feet. Another one bites the dust. You moonwalk your way out of the forest.", choiceTwo: "You follow the bear to his house, leaving the weeping doe behind to enjoy a plate of fresh baked good. You baked as hell."),
+             Story(story: "As you walk to the eagle it swoops down and pecks at your shirt", choiceOne: "You karate chop back down into the ground then jump up and then do a sick elbow drop.", choiceTwo: "You endure its frivilous pecking. no power without pain. metal music plays in the bacground and blood begins to streak across your face."),
              Story(story: "You climb ontop of a tree, like a monkey, and see as far as the eye can see. The mountains pierce the cloads in the distance; rainforest grip the earth in its green mossy grasp; there is a sunlit river that follows the horizon, glimerring in black essense. Among all this you see human tracks, leading in the opposite direction. You know where to go.", choiceOne: "Onwards to adventure! No real adventure is without a ounce of danger.", choiceTwo: "Civilization! Yes back to my computer! Anime, and a soft bed, *frothing in delight*"),
              
              Story(story: "You have chosen: The Realist", choiceOne: "Restart", choiceTwo: "Restart"),
@@ -41,16 +41,16 @@ struct StoryBrain {
     }
     
     func getChoiceOne() -> String {
-        return self.storyElements[self.currentPos].choiceOne
+        return self.storyElements[self.currentPos - 1].choiceOne
     }
     
     func getChoiceTwo() -> String {
-        return self.storyElements[self.currentPos].choiceTwo
+        return self.storyElements[self.currentPos - 1].choiceTwo
     }
     
     mutating func goLeft() {
         if (self.currentPos * 2) >= self.storyElements.count {
-            self.reset()
+            return self.reset()
         }
         self.currentPos = self.currentPos * 2
         
@@ -58,13 +58,13 @@ struct StoryBrain {
     
     mutating func goRight() {
         if (self.currentPos * 2 + 1) >= self.storyElements.count {
-            self.reset()
+            return self.reset()
         }
         self.currentPos = self.currentPos * 2 + 1
         
     }
     
     mutating func reset() {
-        self.currentPos = 0;
+        self.currentPos = 1;
     }
 }
